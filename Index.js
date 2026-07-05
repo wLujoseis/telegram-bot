4b18556d9119b758d05148cb9af41dd4559df5e7
wLujoseis <luissmuddx@gmail.com>
2026-07-04 22:04:00 -0400

    Index.js

--- Index.js
+++ Index.js
@@ -2,14 +2,12 @@ const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// inicio
 bot.start((ctx) => {
-  ctx.reply('Hola 👋 soy tu bot en Railway');
+  ctx.reply('Hola 👋 soy tu bot');
 });
 
-// responder mensajes
 bot.on('text', (ctx) => {
-  ctx.reply(`Recibí: ${ctx.message.text}`);
+  ctx.reply(ctx.message.text);
 });
 
 bot.launch();
